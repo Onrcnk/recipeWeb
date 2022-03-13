327c9b3d16e05c8b0b8b96edb50a6708d0286677
Onrcnk <onrcnk@hotmail.com>
2022-03-13 11:44:03 +0300

    Command objects and converters created.

--- src/main/java/com/onrcnk/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/onrcnk/spring5recipeapp/services/RecipeServiceImpl.java
@@ -1,26 +1,37 @@
 package com.onrcnk.spring5recipeapp.services;
 
+import com.onrcnk.spring5recipeapp.commands.RecipeCommand;
+import com.onrcnk.spring5recipeapp.converters.RecipeCommandToRecipe;
+import com.onrcnk.spring5recipeapp.converters.RecipeToRecipeCommand;
 import com.onrcnk.spring5recipeapp.domain.Recipe;
 import com.onrcnk.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService{
 
     private final RecipeRepository recipeRepository;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe, RecipeToRecipeCommand recipeToRecipeCommand) {
         this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
 
     @Override
     public Set<Recipe> getRecipes() {
-        Set<Recipe> recipeSet = new HashSet<>();
+        log.debug("I'm in the service");
 
+        Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
@@ -35,6 +46,16 @@ public class RecipeServiceImpl implements RecipeService{
         }
 
         return recipeOptional.get();
+    }
+
+    @Override
+    @Transactional
+    public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+        Recipe detachedRecipe = recipeCommandToRecipe.convert(command);
+
+        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+        log.debug("Saved RecipeId:" + savedRecipe.getId());
+        return null;
     }
 
 }
